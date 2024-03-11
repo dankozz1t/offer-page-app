143adb420f8d60fc82c558bf755ba1ca83f75658
Alex-Taylor Danko <67281749+dankozz1t@users.noreply.github.com>
2024-03-11 23:52:03 +0200

    add 3-day trial block

---
+++ components/DayTrialBlock/DayTrialBlock.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import IconLock from '~/components/icons/IconLock.vue';
+</script>
+
+<template>
+  <div class="trial">
+    <h3 class="trial__title">3-day trial for <span class="trial__text--accent">$0.99</span></h3>
+    <p class="trial__subtitle">Then $39.99/week</p>
+
+    <ul class="trial__list">
+      <li class="trial__item">
+        Exclusive access to <span class="trial__text--accent">350+</span> learning programs
+      </li>
+      <li class="trial__item">Personalized course plan</li>
+      <li class="trial__item">Comfy learning schedule made by you</li>
+      <li class="trial__item">
+        <span class="trial__text--accent">24/7</span> tutor support in a secure chat
+      </li>
+      <li class="trial__item">Lifetime access to materials</li>
+    </ul>
+
+    <p class="trial__info">
+      <IconLock />
+      Safe & secure payment
+    </p>
+
+    <p class="trial__disclaimer">
+      $0.99 charged today. If you don't cancel at least 24 hours before the end of the 3-day trial
+      period, you will automatically be charged the full price of $19.99/Month . You can cancel your
+      subscription at any time. By continuing, you indicate that you've read and agree to our Terms
+      & Conditions, Privacy Policy , Money Back , and Subscription Terms .
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$trial-border-color: #cdcdcd;
+$trial-info-border-color: rgba(0, 0, 0, 0.2);
+
+.trial {
+  width: 100%;
+  max-width: 476px;
+  padding: 24px;
+
+  border-radius: 24px;
+  border: 1px solid $trial-border-color;
+  background-color: var(--white-color);
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.06);
+}
+
+.trial__title {
+  margin-bottom: 8px;
+
+  @include font-size(24px, 120%, 800);
+}
+
+.trial__subtitle {
+  margin-bottom: 36px;
+
+  @include font-size(20px, 120%, 600);
+}
+
+.trial__text--accent {
+  font-weight: 700;
+  color: var(--first-accent-color);
+}
+
+.trial__list {
+  margin-bottom: 24px;
+
+  @include font-size(16px, 120%, 400);
+}
+
+.trial__item {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+
+  &::before {
+    content: '';
+    display: flex;
+    width: 24px;
+    height: 24px;
+
+    background-image: url('@/assets/star.svg');
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 18px;
+  }
+}
+
+.trial__info {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  padding: 16px;
+
+  margin-bottom: 24px;
+
+  @include font-size(16px, 100%, 600);
+
+  color: var(--third-accent-color);
+  border: 1px solid $trial-info-border-color;
+  border-radius: 12px;
+}
+
+.trial__disclaimer {
+  text-align: center;
+  opacity: 0.5;
+
+  @include font-size(12px, 120%, 400);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import WelcomeBlock from '~/components/WelcomeBlock/WelcomeBlock.vue';
+import DayTrialBlock from '~/components/DayTrialBlock/DayTrialBlock.vue';
 </script>
 
 <template>
-  <div class="page-container container mx-auto">
-    <WelcomeBlock class="mt-24" />
+  <div class="page-container flex justify-between container mx-auto mt-24">
+    <WelcomeBlock />
+
+    <DayTrialBlock />
   </div>
 </template>
 
